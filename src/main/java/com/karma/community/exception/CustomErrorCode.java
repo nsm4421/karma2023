5d5debf97f8460477bfa94ea1c4eba52bf858adb
nsm <nsm4421@naver.com>
2023-03-20 17:04:18 +0900

    Controller Response, Error
    1. Controller는 RestController를 사용하고, ResponseEntity<Object>를 반환
    - CustomResponse.of(Http상태, 메세지, 데이터) 와 같은 형태로 return
    2. Service 코드에서 Error처리하는 경우
    - throw CustomError.of(에러코드, 메세지) 와 같은 형태로 Error처리
    - 이 경우 GlobalControllerAdivce를 타서 에러처리함
    - GlobalControllerService를 탐

---
+++ src/main/java/com/karma/community/exception/CustomErrorCode.java
@@ -0,0 +1,19 @@
+package com.karma.community.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum CustomErrorCode {
+
+    DUPLICATED_USERNAME(HttpStatus.CONFLICT, "중복된 유저명입니다"),
+    DUPLICATED_NICKNAME(HttpStatus.CONFLICT, "중복된 닉네임입니다"),
+    DUPLICATED_EMAIL(HttpStatus.CONFLICT, "중복된 이메일입니다"),
+    UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "인증되지 않은 접근입니다"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다")
+    ;
+    private final HttpStatus httpStatus;
+    private final String message;
+}
