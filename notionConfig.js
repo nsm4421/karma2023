3ee149f2cd216aaec9574e0d4ddc7c2952e135c2
nsm <nsm4421@naver.com>
2023-04-05 23:35:08 +0900

    Setting for notion API
    - Create setting files
    - Test API for inserting data in database

---
+++ notionConfig.js
@@ -0,0 +1,2 @@
+export const notionSecret = 'secret_XjRkQ90gpTvAIOWWqTMvIUg7sYmLgOTazPNWvTWWaQy'
+export const notionDbId = '3716667f703c484a8631c340ae0ba333'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from 'styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const onClickNotionTest = () => {
+    fetch('/api/test?name=TEST')
+      .then((res) => res.json())
+      .then((data) => alert(data.message))
+      .catch((e) => alert('error'))
+  }
+
   return (
     <>
       <Head>
@@ -14,6 +21,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <button onClick={onClickNotionTest}>Notion Add Test</button>
+
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
