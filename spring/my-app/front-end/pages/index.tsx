82c43249f6860cfe0c28860e7a029ab8b8a438db
nsm <nsm4421@naver.com>
2023-04-23 16:32:00 +0900

    Front-End 프로젝트 생성
    - Next JS
    - Typescript

---
+++ spring/my-app/front-end/pages/index.tsx
@@ -0,0 +1,12 @@
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export default function Home() {
+
+  return (
+    <main>
+      <h1>Test</h1>
+      <FontAwesomeIcon icon={faCoffee} />
+    </main>
+  )
+}
